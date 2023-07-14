0541fc1ccadaf1590ab1db9a4611cfb2ffb93054
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-07-14 14:17:00 -0300

    feat: implemente class queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,54 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.list = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        """
+        Return the length of the list.
+
+        :return: The length of the list.
+        :rtype: int
+        """
+        return len(self.list)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        """
+        Adds a value to the end of the list.
+
+        Args:
+            value: The value to be added to the list.
+
+        Returns:
+            None
+        """
+        return self.list.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        """
+        Remove and return the element at the front of the queue.
+
+        Returns:
+            The element at the front of the queue.
+
+        Raises:
+            IndexError: If the queue is empty.
+        """
+        return self.list.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        """
+        Search for an element in the list at the given index.
+
+        Parameters:
+            index (int): The index of the element to search for.
+
+        Returns:
+            object: The element found at the given index.
+
+        Raises:
+            IndexError: If the index is invalid or does not exist in the list.
+        """
+        if index < 0 or index >= len(self.list):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self.list[index]
