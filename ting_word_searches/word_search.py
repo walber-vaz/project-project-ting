9b3d7a64eb24d136cca6ba36afe00bfe3e961f22
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-07-14 16:36:08 -0300

    feat: gen docstrings

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,4 +1,22 @@
 def exists_word(word, instance):
+    """
+    Generate a list of dictionaries representing occurrences of a given word
+    in a list of files.
+
+    Args:
+        word (str): The word to search for.
+        instance (Instance): An instance object containing a list of files.
+
+    Returns:
+        list: A list of dictionaries representing the occurrences of the word
+        in the files. Each dictionary contains the following keys:
+            - palavra (str): The word.
+            - arquivo (str): The name of the file.
+            - ocorrencias (list): A list of dictionaries representing the
+            occurrences of the word in the file. Each dictionary contains
+            the following keys:
+                - linha (int): The line number where the word occurs.
+    """
     archives = [
         {
             "palavra": word,
@@ -22,6 +40,24 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
+    """
+    Search for occurrences of a given word in a list of files.
+
+    Parameters:
+        word (str): The word to search for.
+        instance (Instance): An instance object containing a list of file data.
+
+    Returns:
+        list: A list of dictionaries, each containing information about a file
+        where the word was found. Each dictionary has the following keys:
+            - palavra (str): The searched word.
+            - arquivo (str): The name of the file.
+            - ocorrencias (list): A list of dictionaries, each containing the
+            line number and content where the word was found. Each dictionary
+              has the following keys:
+                - linha (int): The line number.
+                - conteudo (str): The line content.
+    """
     results = []
 
     for file_data in instance._list:
